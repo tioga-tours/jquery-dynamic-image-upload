6833e594ebe6603fbd05076bd82979029721d2c5
Martin Winkel <martin_winkel@hotmail.com>
2016-04-26 15:29:23 +0200

    Add upload handler example

---
+++ upload.php
@@ -0,0 +1,8 @@
+<?php
+
+$exifData = $_POST['exif'];
+$customData = $_POST['customData'];
+$name = $_POST['name'];
+
+$withoutMime = substr($dataUrl, strpos($$_POST['src', ",") + 1);
+$src = base64_decode(withoutMime);
